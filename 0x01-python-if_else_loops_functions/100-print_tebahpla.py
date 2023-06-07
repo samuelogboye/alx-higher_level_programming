7869aaccfa7703e90151546490dd4ececb1145ca
Samuel <ogboyesam@gmail.com>
2023-06-06 18:15:14 -0700

    A program that prints the ASCII alphabet in reverse order

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i)}" if i % 2 == 0 else f"{chr(i).upper()}", end="")
+for letter in range(ord('z'), ord('a') - 1, -1):
+    if letter % 2 == 0:
+        i = 0
+    else:
+        i = 32
+    print("{}".format(chr(letter - i)), end="")
