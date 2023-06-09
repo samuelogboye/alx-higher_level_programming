914cb9139b058c9093f679a70fa22029e57d427b
Samuel <ogboyesam@gmail.com>
2023-06-09 05:19:15 -0700

    A program is translated from byetcode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
