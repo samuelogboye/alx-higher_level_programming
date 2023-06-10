83911ffdd333fa3d80146f6fe7c4e88955416997
Samuel <ogboyesam@gmail.com>
2023-06-10 07:58:46 -0700

    update a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """A function that adds 2 tuples"""
-    a_value_one = 0
-    a_value_two = 0
-    b_value_one = 0
-    b_value_two = 0
 
-    try:
-        a_value_one = tuple_a[0]
-        a_value_two = tuple_a[1]
-        b_value_one = tuple_b[0]
-        b_value_two = tuple_b[1]
-    except IndexError:
-        pass
+    len_a, len_b = len(tuple_a), len(tuple_b)
 
-    if a_value_one == 0:
-        a_value_one = 0
-    if a_value_two == 0:
-        a_value_two = 0
-    if b_value_one == 0:
-        b_value_one = 0
-    if b_value_two == 0:
-        b_value_two = 0
+    if len_a < 2:
+        if len_a == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
 
-    value_one = a_value_one + b_value_one
-    value_two = a_value_two + b_value_two
+    if len_b < 2:
+        if len_b == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    value_one = tuple_a[0] + tuple_b[0]
+    value_two = tuple_a[1] + tuple_b[1]
     return value_one, value_two
