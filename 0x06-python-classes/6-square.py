8dcbe8ca899a5c7307445f44138cb58c52fd135a
Samuel Ogboye <125566090+samuelogboye@users.noreply.github.com>
2023-06-28 03:33:41 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,13 +5,19 @@
 
 class Square:
     """Represent a square"""
-    def __init__(self, size=0, position=(0, 0)):
 
+    def __init__(self, size=0, position=(0, 0)):
+         """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """Get/set the size of the square."""
         return self.__size
 
     @size.setter
