1a02a33125add38bdab3785ba98ff3ad5dad870c
Samuel Ogboye <125566090+samuelogboye@users.noreply.github.com>
2023-06-28 03:29:29 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,5 +50,7 @@ class Square:
         else:
             for _ in range(self.__position[1]):
                 print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
+                print("")
