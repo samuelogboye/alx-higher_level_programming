e45458d39f3e79b31b387ffa2bd839a0046ec2f7
Samuel <ogboyesam@gmail.com>
2023-06-27 19:23:50 -0700

    public attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         return self.__size * self.__size
 
     def my_print(self):
-         """Print the square with the # character."""
+        """Print the square with the # character."""
         if self.__size == 0:
             print()
         else:
