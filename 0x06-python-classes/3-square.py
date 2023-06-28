72b69b16122d6f3159782328a204984091116fdd
Samuel <ogboyesam@gmail.com>
2023-06-27 18:12:58 -0700

    public instance method implemented

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,15 +6,13 @@
 class Square:
     """Represent a square"""
     def __init__(self, size=0):
-        self.__size = size
 
-    def get_size(self):
-        return self.__size
-
-    def set_size(self, size):
-        if self.__size != isdigit():
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
