3506789bd4374d40f66ce3f5da69e9758c297578
Samuel <ogboyesam@gmail.com>
2023-06-27 19:35:14 -0700

    public attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,7 +7,7 @@ class Square:
     """Represent a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-         """Initialize a new square.
+        """Initialize a new square.
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
