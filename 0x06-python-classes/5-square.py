9ad27d20672b0c595ce801ece6bf48e082c4705c
Samuel <ogboyesam@gmail.com>
2023-06-27 18:34:22 -0700

    implemented more publice instance method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,12 +15,19 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
     def area(self):
         return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
