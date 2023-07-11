1af31a9c653b6e0f3cb735bd3c5a63651d8681b6
Samuel <ogboyesam@gmail.com>
2023-07-11 16:38:33 -0700

    a function that reads a text file UTF8 and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 
 
 def read_file(filename=""):
-    with open("my_file_0.txt", 'r', encoding="utf-8") as f:
+    with open("my_file_0.txt", encoding="utf-8") as f:
         for line in f:
             print(line)
