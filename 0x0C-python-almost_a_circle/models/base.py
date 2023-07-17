9977b7127b121f5fb6a9cf601fc4783b9eddf187
Samuel <ogboyesam@gmail.com>
2023-07-17 06:24:06 -0700

    JSON implementation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The Base class"""
 
+import json
 
 class Base:
     """Defining Class Base"""
@@ -14,3 +15,25 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ a function that returns the JSON representation of an
+        object (list_dictionaries)"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+
+        if (type(list_dictionaries) != list or not
+                all(type(i) == dict for i in list_dictionaries)):
+            raise TypeError("list_dictionaries must be a list of dictionaries")
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save JSON representation to file"""
+        file_name = cls.__name__ + ".json"
+        with open(file_name, "w") as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                list_dicts = [i.to_dictionary() for i in list_objs]
+                json_file.write(Base.to_json_string(list_dicts))
