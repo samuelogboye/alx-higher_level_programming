d8a89d53e3c4f928f1650fc8fd2d605f32a9abee
Samuel <ogboyesam@gmail.com>
2023-07-17 07:16:15 -0700

    dictionary to Instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -48,3 +48,15 @@ class Base:
         if type(json_string) != str:
                 raise TypeError("json_string must be a string")
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        # create an instance of an existing class
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+
+        dummy.update(**dictionary)
+        return dummy
