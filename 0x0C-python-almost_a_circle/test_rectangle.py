4b6deba7732974a81204d82ad2390d3b114c4f76
Samuel <ogboyesam@gmail.com>
2023-07-18 07:26:15 -0700

    rectangle test updated

--- 0x0C-python-almost_a_circle/test_rectangle.py
+++ 0x0C-python-almost_a_circle/test_rectangle.py
@@ -4,7 +4,12 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangleMethods(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.rectangle = Rectangle(5, 10, 2, 3, 1)
+
+    def test_area(self):
+        self.assertEqual(self.rectangle.area(), 50)
 
     def test_rectangle_attributes(self):
         """Test rectangle attributes"""
@@ -26,45 +31,27 @@ class TestRectangleMethods(unittest.TestCase):
         self.assertEqual(r.x, 3)
         self.assertEqual(r.y, 4)
 
-    def test_rectangle_area(self):
-        """Test rectangle area calculation"""
-        r = Rectangle(5, 10)
-        self.assertEqual(r.area(), 50)
+    def test_str(self):
+        expected_output = "[Rectangle] (1) 2/3 - 5/10"
+        self.assertEqual(str(self.rectangle), expected_output)
 
-        r.width = 8
-        r.height = 15
-        self.assertEqual(r.area(), 120)
+    def test_update(self):
+        self.rectangle.update(2, 7, 12, 4, 5)
+        self.assertEqual(self.rectangle.width, 7)
+        self.assertEqual(self.rectangle.height, 12)
+        self.assertEqual(self.rectangle.x, 4)
+        self.assertEqual(self.rectangle.y, 5)
 
-    def test_rectangle_str(self):
-        """Test rectangle string representation"""
-        r = Rectangle(7, 7, 1, 1, 200)
-        self.assertEqual(str(r), "[Rectangle] (200) 1/1 - 7/7")
-
-    def test_rectangle_update(self):
-        """Test rectangle update method"""
-        r = Rectangle(10, 20, 1, 2, 300)
-
-        r.update(50)
-        self.assertEqual(r.id, 50)
-
-        r.update(50, 30)
-        self.assertEqual(r.width, 30)
-
-        r.update(50, 30, 40)
-        self.assertEqual(r.height, 40)
-
-        r.update(50, 30, 40, 50)
-        self.assertEqual(r.x, 50)
-
-        r.update(50, 30, 40, 50, 60)
-        self.assertEqual(r.y, 60)
-
-    def test_rectangle_to_dictionary(self):
-        """Test rectangle to_dictionary method"""
-        r = Rectangle(5, 10, 2, 3, 400)
-        expected_dict = {'id': 400, 'width': 5, 'height': 10, 'x': 2, 'y': 3}
-        self.assertEqual(r.to_dictionary(), expected_dict)
+    def test_to_dictionary(self):
+        expected_output = {
+            'id': 1,
+            'width': 5,
+            'height': 10,
+            'x': 2,
+            'y': 3
+        }
+        self.assertEqual(self.rectangle.to_dictionary(), expected_output)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
