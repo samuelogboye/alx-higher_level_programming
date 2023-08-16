5233144598297744b4aaf9a674552a045851830d
Samuel <ogboyesam@gmail.com>
2023-08-15 18:25:31 -0700

    a script that displays the top 3 of cities temperature

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending)
 SELECT `city`, AVG(`values`) AS `av_temp`
-FROM `tempratures
+FROM `temperatures
 GROUP BY `city`
 ORDER BY `av_temp` DESC;
