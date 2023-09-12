17ea20ad17b7089bbb0964762147537f9c507fd8
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-12 03:57:31 +0100

    function that increments and calls a function.

--- 0x12-javascript-warm_up/a.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const a = [4, 2, 5, 3, 6, 7, 8, 4, 10, 9, 1];
-// Sort the array in ascending order
-a.sort(function (a, b) {
-  return a - b;
-});
-console.log(a);
-console.log(a[a.length - 2]);
