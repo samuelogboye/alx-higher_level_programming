404e18f8bde98e530290c11c617f33edb1e32a9b
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-12 03:36:11 +0100

    a cript to replace the value 12 with 89:

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// This script replaces the value 12 with 89:
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/*
+  YOUR CODE HERE
+  */
+myObject.value = 89;
+console.log(myObject);
