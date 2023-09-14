880bea6215b34814fae10135199562559ce47f37
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-14 05:08:05 +0100

    class Rectangle that defines a rectangle: task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// a class Rectangle that defines a rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      // this. to make the variable public
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // method to print the rectangle using the character X
+  print () {
+    let row;
+    for (row = 0; row < this.height; row++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  // method to rotate the rectangle
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  // method to double the rectangle
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
