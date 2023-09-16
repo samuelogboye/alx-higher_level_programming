229205ac376d110b0ff8d8feb746e260e727f996
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-17 00:17:49 +0100

    preventing SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -31,16 +31,10 @@ if __name__ == '__main__':
     cur = db.cursor()
 
     # Create the SQL query with string formatting
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'\
-        ORDER BY id ASC".format(argv[4])
-
     #  to prevent SQL injection
     cur.execute(
         "SELECT * FROM states WHERE name LIKE\
                     BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
-
-    # Execute the query
-    cur.execute(query)
 
     rows = cur.fetchall()
 
