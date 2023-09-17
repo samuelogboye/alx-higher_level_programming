7dd5cb900edb5e52625925205e75e495886e07bd
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-17 01:15:26 +0100

    a python file that contains the class definition of a State and an instance Base = declarative_base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Write a python file that contains the class definition of a State and an
+# instance Base = declarative_base():
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A class that inherits from Base and links to the MySQL table states
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
