47487fddf9a9bdfa52efc0484ee1ef81a39f3490
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-28 19:12:13 -0700

    peak refactored more

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,13 +12,18 @@ def find_peak(list_of_integers):
     if list_of_integers is None or list_of_integers == []:
         return None
 
-    right, left = 0, len(list_of_integers) - 1
-
-    while right < left:
-        num = (right + left) // 2
-        if list_of_integers[num] < list_of_integers[num + 1]:
-            right = num + 1
-        else:
-            left = num
-
-    return list_of_integers[right]
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
