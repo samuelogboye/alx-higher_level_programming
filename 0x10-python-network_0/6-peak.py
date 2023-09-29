de84569def4e271b1406353672cf8d52f2993103
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-28 19:10:51 -0700

    peak refactored

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,7 +15,7 @@ def find_peak(list_of_integers):
     right, left = 0, len(list_of_integers) - 1
 
     while right < left:
-        num = ((right + left) // 2) + 0
+        num = (right + left) // 2
         if list_of_integers[num] < list_of_integers[num + 1]:
             right = num + 1
         else:
