7f3167b923947e26f4437110be3240ba0f346fff
Samuel Ogboye <ogboyesam@gmail.com>
2023-09-28 18:43:40 -0700

    a function that finds a peak in a list of unsorted integers.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    Function that finds the peak of an unsorted
+    list
+    """
+    if not list_of_integers:
+        return None
+
+    right, left = 0, len(list_of_integers) - 1
+
+    while right < left:
+        num = (right + left) // 2
+        if list_of_integers[num] < list_of_integers[num + 1]:
+            right = num + 1
+        else:
+            left = num
+
+    return list_of_integers[right]
