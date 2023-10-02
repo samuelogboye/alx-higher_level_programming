6fc639ccc416ccc1c934cb19594abe09c5f250c3
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-02 16:32:19 -0700

    change

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@ ok
 """
 import urllib.request
 
+
 if __name__ == '__main__':
     """
     urllib library to fetch urls
     """
-
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         html = response.read()
