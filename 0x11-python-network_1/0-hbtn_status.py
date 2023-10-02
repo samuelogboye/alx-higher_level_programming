c28da5bd345b70aed1c979ced37fd47a5dd83b3f
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-02 23:40:31 +0100

    task 0 modified6

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     urllib library to fetch urls
     """
 
-    request = urllib.request.Request("https://intranet.hbtn.io/statu")
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         html = response.read()
         print("Body response:")
