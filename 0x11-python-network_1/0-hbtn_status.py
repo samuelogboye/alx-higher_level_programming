ae95e27960ab0743de26bfd6e4aa45936507c580
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-02 23:56:11 +0100

    task 0 modified7

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -15,4 +15,4 @@ if __name__ == '__main__':
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
