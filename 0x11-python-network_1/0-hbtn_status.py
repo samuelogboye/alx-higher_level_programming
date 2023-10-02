543a0e84c05905bbcaf6291eb8589a94b6ecf982
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-02 15:02:30 +0100

    a python script that fetch response from a website1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""
+Fetches https://intranet.hbtn.io/status
+"""
+
 import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode('utf8')))
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
