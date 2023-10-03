3fa430ee12ebcd7bcb7af78856bb236665dee49b
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-03 00:59:45 -0700

    a python script to get the last 10 commits of a github repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""A script that takes 2 arguments repository name and owner name
+then displays the commits of the repository
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
