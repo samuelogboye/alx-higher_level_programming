57f9e3290abf43c3f1f499c90449ca707f52180d
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-24 15:49:36 +0100

    a js script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", function (error, content) {
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   console.log(error || content);
 });
