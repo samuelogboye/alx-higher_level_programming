ae6503ee1cc215e868e9f0895f2bbc242c8f5693
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-24 21:12:26 +0100

    a js script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    for (const character of data.characters) {
+      request(character, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          const data = JSON.parse(body);
+          console.log(data.name);
+        }
+      });
+    }
+  }
+});
