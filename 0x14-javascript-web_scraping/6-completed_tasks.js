dd77e2fcfb53dd9a42640f19e433b42fdc6ce439
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-24 20:54:50 +0100

    a js script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const completedTasks = {};
+
+request(process.argv[2], (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    for (const user of data) {
+      if (user.completed === true) {
+        if (user.userId in completedTasks) {
+          completedTasks[user.userId]++;
+        } else {
+          completedTasks[user.userId] = 1;
+        }
+      }
+    }
+    console.log(completedTasks);
+  }
+});
