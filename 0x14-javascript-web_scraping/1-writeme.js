7f311a0be10a4c8520c104507352c7dfa7dbc571
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-24 16:11:44 +0100

    a js script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2]; // Get the file path from the command line arguments
+const contentToWrite = process.argv[3]; // Get the content to write from the command line arguments
+
+fs.writeFile(filePath, contentToWrite, 'utf8', (error) => {
+  if (error) {
+    console.error('Error writing to the file:', error);
+  } else {
+    console.log('File has been written.');
+  }
+});
