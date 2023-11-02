e239a3d4e3da1a5495aa6ef1b739655c9c661b7b
Samuel Ogboye <ogboyesam@gmail.com>
2023-11-02 05:33:07 +0100

    JavaScript script that fetches from an api

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  $.getJSON(
+    "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+    function (data) {
+      $("#character").text(data.name);
+    }
+  );
+});
