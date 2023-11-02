66e5bc45f80bbde449714266c04c5e1bf742f2cb
Samuel Ogboye <ogboyesam@gmail.com>
2023-11-02 05:44:41 +0100

    a JavaScript script that fetches and prints how to say “Hello” depending on the language

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+$("document").ready(function () {
+  $("INPUT#btn_translate").click(translate);
+  $("INPUT#language_code").focus(function () {
+    $(this).keydown(function (enter) {
+      if (enter.keycode === 13) {
+        translate();
+      }
+    });
+  });
+});
+
+function translate() {
+  const url = "https://www.fourtonfish.com/hellosalut/?";
+  $.get(
+    url + $.param({ lang: $("INPUT#language_code").val() }),
+    function (data) {
+      $("DIV#hello").html(data.hello);
+    }
+  );
+}
